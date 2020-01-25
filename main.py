9e712476326eef7cec70c7897d71ba0bbde4a64a
KDube1 <krishna.dube1@gmail.com>
2020-01-25 15:17:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ window.title("ConUHacks")
 
 lbl = Label(window, text="Hello", font=("Helvetica", 50))
 
-lbl.grid(column=0, row=2)
+lbl.grid(column=0, row=0)
 
 window.geometry('800x500')
 
-btn = Button(window, text="Click Me")
+rad1 = Radiobutton(window, text='First', value=1)
 
-btn.grid(column=0, row=1)
+rad2 = Radiobutton(window, text='Second', value=2)
+
+rad3 = Radiobutton(window, text='Third', value=3)
+
+rad1.grid(column=0, row=1)
+
+rad2.grid(column=1, row=1)
+
+rad3.grid(column=2, row=1)
 
 window.mainloop()
