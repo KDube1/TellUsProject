cf85dd235f7392a796e97125178dad90a7d53d2d
minhradz <minhradz@gmail.com>
2020-01-25 22:18:35 -0500

    updated hmtl, css, and python code, remember to install the module

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-from tkinter import *
-
-window = Tk()
-
-window.title("ConUHacks")
-
-lbl = Label(window, text="Hello", font=("Helvetica", 50))
-
-lbl.grid(column=0, row=2)
-
-window.geometry('800x500')
-
-btn = Button(window, text="Click Me")
-
-btn.grid(column=0, row=1)
-
-window.mainloop()
+import eel
+eel.init('web')
+eel.start('main.html')
